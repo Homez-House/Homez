b60468791f386d31e505b761156627ffed65684c
eona1301 <eona1301@gmail.com>
2021-05-24 01:50:16 +0900

    member rest api test update

--- src/main/java/com/ssafy/homez/controller/MemberController.java
+++ src/main/java/com/ssafy/homez/controller/MemberController.java
@@ -6,8 +6,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,7 +51,30 @@ public class MemberController {
 
 		if (dto == null) {
 			memberService.insert(memberDto);
-			System.out.println(memberDto);
+			System.out.println("|| insert : " + memberDto);
+			return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+		} else
+			return new ResponseEntity<Integer>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@PutMapping(value = "/member/{memberId}")
+	public ResponseEntity<Integer> update(@PathVariable String memberId, @RequestBody MemberDto memberDto) {
+		MemberDto dto = memberService.detail(memberId);
+
+		if (dto != null) {
+			memberService.update(memberDto);
+			System.out.println("|| update : " + memberDto);
+			return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+		} else
+			return new ResponseEntity<Integer>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@DeleteMapping(value="/member/{memberId}")
+	public ResponseEntity<Integer> delete(@PathVariable String memberId) {
+		MemberDto dto = memberService.detail(memberId);
+		if(dto != null) {
+			memberService.delete(memberId);
+			System.out.println("|| delete : " + memberId);
 			return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
 		}else
 			return new ResponseEntity<Integer>(FAIL, HttpStatus.INTERNAL_SERVER_ERROR);
