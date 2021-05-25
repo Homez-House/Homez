06ff8f177ea0f8687f835395b900e2463904e040
eona1301 <eona1301@gmail.com>
2021-05-25 21:21:12 +0900

    back merge

--- src/main/java/com/ssafy/homez/controller/HouseController.java
+++ src/main/java/com/ssafy/homez/controller/HouseController.java
@@ -19,65 +19,65 @@ import com.ssafy.homez.service.HouseService;
 
 // VUE cli mode 개발 대응
 @CrossOrigin(origins ="http://localhost:5500", allowCredentials="true", allowedHeaders="*",
-		methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.HEAD, RequestMethod.OPTIONS}
+        methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.HEAD, RequestMethod.OPTIONS}
 )
 @RestController
 public class HouseController {
-	
-	@Autowired
-	HouseService houseService;
-	
-	@GetMapping(value="/gugun")
-	public ResponseEntity<List<Map<String, String>>> getGugunList(){
-		System.out.println("|| gugun 리스트 확인하기");
-		List<Map<String, String>> list = null;
-		
-		list = houseService.getGugunList();
-		
-		for(int i=0,size=list.size(); i<size; i++ ) {
-			Map<String,String> map = list.get(i);
-			for(String key: map.keySet()) {
-				System.out.print( key + " " + map.get(key) );
-			}
-			System.out.println();
-		}
-		return new ResponseEntity<List<Map<String, String>>>(list, HttpStatus.OK);
-	}
-	
-	@GetMapping(value="/gugun/{gugunCode}")
-	public ResponseEntity<List<Map<String, String>>> getDongList(@PathVariable String gugunCode){
-		System.out.println("|| gugunCode를 기반으로 dong 리스트 확인하기 : " + gugunCode);
-		List<Map<String, String>> list = null;
-		
-		list = houseService.getDongList(gugunCode);
-		
-		for(int i=0,size=list.size(); i<size; i++ ) {
-			Map<String,String> map = list.get(i);
-			for(String key: map.keySet()) {
-				System.out.print( key + " " + map.get(key) );
-			}
-			System.out.println();
-		}
-		
-		return new ResponseEntity<List<Map<String, String>>>(list, HttpStatus.OK);
-	}
-	
-	
-	@GetMapping(value = "/houses")
-	public ResponseEntity<List<HouseDto>> searchByDong(HouseParamDto houseParamDto) {
-		
-		List<HouseDto> list = null;
-		
-		if (houseParamDto.getHouseSearchType().equals("dongName")) {
-			list = houseService.searchByDong(houseParamDto);
-		} else if (houseParamDto.getHouseSearchType().equals("aptName")) {
-			list = houseService.searchByAptName(houseParamDto);
-		}
-		
-		for(HouseDto house : list) {
-			System.out.println(house);
-		}
-		
-		return new ResponseEntity<List<HouseDto>>(list, HttpStatus.OK);
-	}
+    
+    @Autowired
+    HouseService houseService;
+    
+    @GetMapping(value="/gugun")
+    public ResponseEntity<List<Map<String, String>>> getGugunList(){
+        System.out.println("|| gugun 리스트 확인하기");
+        List<Map<String, String>> list = null;
+        
+        list = houseService.getGugunList();
+        
+        for(int i=0,size=list.size(); i<size; i++ ) {
+            Map<String,String> map = list.get(i);
+            for(String key: map.keySet()) {
+                System.out.print( key + " " + map.get(key) );
+            }
+            System.out.println();
+        }
+        return new ResponseEntity<List<Map<String, String>>>(list, HttpStatus.OK);
+    }
+    
+    @GetMapping(value="/gugun/{gugunCode}")
+    public ResponseEntity<List<Map<String, String>>> getDongList(@PathVariable String gugunCode){
+        System.out.println("|| gugunCode를 기반으로 dong 리스트 확인하기 : " + gugunCode);
+        List<Map<String, String>> list = null;
+        
+        list = houseService.getDongList(gugunCode);
+        
+        for(int i=0,size=list.size(); i<size; i++ ) {
+            Map<String,String> map = list.get(i);
+            for(String key: map.keySet()) {
+                System.out.print( key + " " + map.get(key) );
+            }
+            System.out.println();
+        }
+        
+        return new ResponseEntity<List<Map<String, String>>>(list, HttpStatus.OK);
+    }
+    
+    
+    @GetMapping(value = "/houses")
+    public ResponseEntity<List<HouseDto>> searchByDong(HouseParamDto houseParamDto) {
+        
+        List<HouseDto> list = null;
+        System.out.println(houseParamDto);
+        if (houseParamDto.getHouseSearchType().equals("dongName")) {
+            list = houseService.searchByDong(houseParamDto);
+        } else if (houseParamDto.getHouseSearchType().equals("aptName")) {
+            list = houseService.searchByAptName(houseParamDto);
+        }
+        
+        for(HouseDto house : list) {
+            System.out.println(house);
+        }
+        
+        return new ResponseEntity<List<HouseDto>>(list, HttpStatus.OK);
+    }
 }
--- src/main/java/com/ssafy/homez/dto/HouseDto.java
+++ src/main/java/com/ssafy/homez/dto/HouseDto.java
@@ -1,138 +1,119 @@
 package com.ssafy.homez.dto;
 
 public class HouseDto {
-	private int no;
-	private String dongName;
-	private String houseName;
-	private String gugunCode;
-	private String dealAmount;
-	private String buildYear;
-	private String dealYear;
-	private String dealMonth;
-	private String dealDay;
-	private String area;
-	private String floor;
-	private String jibun;
-	private String lat;
-	private String lng;
+    private int no;
+    private String dongName;
+    private String gugunName;
+    private String houseName;
+    private String gugunCode;
+    private String dealAmount;
+    private String buildYear;
+    private String dealYear;
+    private String dealMonth;
+    private String dealDay;
+    private String area;
+    private String floor;
+    private String jibun;
+    private String lat;
+    private String lng;
+    
+    public int getNo() {
+        return no;
+    }
+    public void setNo(int no) {
+        this.no = no;
+    }
+    public String getDongName() {
+        return dongName;
+    }
+    public void setDongName(String dongName) {
+        this.dongName = dongName;
+    }
+    public String getGugunName() {
+        return gugunName;
+    }
+    public void setGugunName(String gugunName) {
+        this.gugunName = gugunName;
+    }
+    public String getHouseName() {
+        return houseName;
+    }
+    public void setHouseName(String houseName) {
+        this.houseName = houseName;
+    }
+    public String getGugunCode() {
+        return gugunCode;
+    }
+    public void setGugunCode(String gugunCode) {
+        this.gugunCode = gugunCode;
+    }
+    public String getDealAmount() {
+        return dealAmount;
+    }
+    public void setDealAmount(String dealAmount) {
+        this.dealAmount = dealAmount;
+    }
+    public String getBuildYear() {
+        return buildYear;
+    }
+    public void setBuildYear(String buildYear) {
+        this.buildYear = buildYear;
+    }
+    public String getDealYear() {
+        return dealYear;
+    }
+    public void setDealYear(String dealYear) {
+        this.dealYear = dealYear;
+    }
+    public String getDealMonth() {
+        return dealMonth;
+    }
+    public void setDealMonth(String dealMonth) {
+        this.dealMonth = dealMonth;
+    }
+    public String getDealDay() {
+        return dealDay;
+    }
+    public void setDealDay(String dealDay) {
+        this.dealDay = dealDay;
+    }
+    public String getArea() {
+        return area;
+    }
+    public void setArea(String area) {
+        this.area = area;
+    }
+    public String getFloor() {
+        return floor;
+    }
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+    public String getJibun() {
+        return jibun;
+    }
+    public void setJibun(String jibun) {
+        this.jibun = jibun;
+    }
+    public String getLat() {
+        return lat;
+    }
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+    public String getLng() {
+        return lng;
+    }
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
 
-	public int getNo() {
-		return no;
-	}
+    @Override
+    public String toString() {
+        return "HouseDto [no=" + no + ", dongName=" + dongName + ", gugunName=" + gugunName + ", houseName=" + houseName
+                + ", gugunCode=" + gugunCode + ", dealAmount=" + dealAmount + ", buildYear=" + buildYear + ", dealYear="
+                + dealYear + ", dealMonth=" + dealMonth + ", dealDay=" + dealDay + ", area=" + area + ", floor=" + floor
+                + ", jibun=" + jibun + ", lat=" + lat + ", lng=" + lng + "]";
+    }
 
-	public void setNo(int no) {
-		this.no = no;
-	}
-
-	public String getDongName() {
-		return dongName;
-	}
-
-	public void setDongName(String dongName) {
-		this.dongName = dongName;
-	}
-
-	public String getHouseName() {
-		return houseName;
-	}
-
-	public void setHouseName(String houseName) {
-		this.houseName = houseName;
-	}
-
-	public String getGugunCode() {
-		return gugunCode;
-	}
-
-	public void setGugunCode(String gugunCode) {
-		this.gugunCode = gugunCode;
-	}
-
-	public String getDealAmount() {
-		return dealAmount;
-	}
-
-	public void setDealAmount(String dealAmount) {
-		this.dealAmount = dealAmount;
-	}
-
-	public String getBuildYear() {
-		return buildYear;
-	}
-
-	public void setBuildYear(String buildYear) {
-		this.buildYear = buildYear;
-	}
-
-	public String getDealYear() {
-		return dealYear;
-	}
-
-	public void setDealYear(String dealYear) {
-		this.dealYear = dealYear;
-	}
-
-	public String getDealMonth() {
-		return dealMonth;
-	}
-
-	public void setDealMonth(String dealMonth) {
-		this.dealMonth = dealMonth;
-	}
-
-	public String getDealDay() {
-		return dealDay;
-	}
-
-	public void setDealDay(String dealDay) {
-		this.dealDay = dealDay;
-	}
-
-	public String getArea() {
-		return area;
-	}
-
-	public void setArea(String area) {
-		this.area = area;
-	}
-
-	public String getFloor() {
-		return floor;
-	}
-
-	public void setFloor(String floor) {
-		this.floor = floor;
-	}
-
-	public String getJibun() {
-		return jibun;
-	}
-
-	public void setJibun(String jibun) {
-		this.jibun = jibun;
-	}
-
-	public String getLat() {
-		return lat;
-	}
-
-	public void setLat(String lat) {
-		this.lat = lat;
-	}
-
-	public String getLng() {
-		return lng;
-	}
-
-	public void setLng(String lng) {
-		this.lng = lng;
-	}
-
-	@Override
-	public String toString() {
-		return "HouseDto [no=" + no + ", dongName=" + dongName + ", houseName=" + houseName + ", gugunCode=" + gugunCode
-				+ ", dealAmount=" + dealAmount + ", buildYear=" + buildYear + ", dealYear=" + dealYear + ", dealMonth="
-				+ dealMonth + ", dealDay=" + dealDay + ", area=" + area + ", floor=" + floor + ", jibun=" + jibun
-				+ ", lat=" + lat + ", lng=" + lng + "]";
-	}
 }
